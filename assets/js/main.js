cd679bf7604620c4651594c31e5d727200458b3a
judsonpaiva <judson.paiva@hotmail.com>
2022-08-03 01:20:13 +0100

    funções de cálculo do IMC

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,48 @@ const form = document.querySelector("#form");
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    setResultado("Olá Mundo!");
+
+    const inputPeso = e.target.querySelector("#peso");
+    const inputAltura = e.target.querySelector("#altura");
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado('Peso inválido!', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida!', false);
+        return;
+    }
+
+
+    const imc = getImc(peso, altura);
+
+    setResultado(imc, true);
+
 });
 
+function getImc(peso, altura) {
+    return (peso / altura ** 2).toFixed(2);
+}
 
-function setResultado(msg) {
+
+
+
+function criarP() {
+    const p = document.createElement("p");
+    return p;
+}
+
+function setResultado(msg, isValid) {
     const resultado = document.querySelector("#resultado");
     resultado.innerHTML = '';
 
-    const p = document.createElement("p");
+    const p = criarP();
+    p.innerHTML = msg;
     p.classList.add('paragrafo-resultado');
-    p.innerHTML = 'Qualquer coisa';
     resultado.appendChild(p);
 }
